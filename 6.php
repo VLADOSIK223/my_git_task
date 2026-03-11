8932e168996c245b69be8b27c78a8cc00710e845
Vladik <mokki.51.mokki@gmail.com>
2026-03-11 13:42:27 +0300

    первый коммит

---
+++ 6.php
@@ -0,0 +1,27 @@
+<?php
+$input = readline("Введите числа через пробел: ");
+$arr = explode(" ", $input);
+
+if (count($arr) < 2) {
+    die("Нужно хотя бы два числа!");
+}
+
+$isZigzag = true;
+
+for ($i = 1; $i < count($arr) - 1; $i++) {
+   
+    $isPeak = ($arr[$i] > $arr[$i-1] && $arr[$i] > $arr[$i+1]);
+    $isValley = ($arr[$i] < $arr[$i-1] && $arr[$i] < $arr[$i+1]);
+
+    if (!$isPeak && !$isValley) {
+        $isZigzag = false;
+        break;
+    }
+}
+
+if ($isZigzag) {
+    echo "Это зигзаг! ";
+} else {
+    echo "Это не зигзаг. ";
+}
+?>
